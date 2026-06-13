7bcb9834db9d27f0fdec04ffefc9b0269e795874
Daniel Larsson <daniel.larsson@grouptalk.com>
2026-06-13 21:57:53 +0200

    Display the git commit id

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
+import { execSync } from 'node:child_process'
 import { defineConfig } from 'vite'
+
+const gitCommit = execSync('git rev-parse --short HEAD').toString().trim()
 
 export default defineConfig({
   base: '',
+  define: {
+    __APP_COMMIT__: JSON.stringify(gitCommit),
+  },
 })
